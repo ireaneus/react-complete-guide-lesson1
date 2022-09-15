1b2b63b2b0543db6e0f6606a2cb0112e65b09026
ireaneus <bwdave@protonmail.com>
2022-09-15 13:58:00 -0600

    new expense form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './styles/App.css';
-import Expenses from './components/Features/Expenses';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   const expenses = [
@@ -35,14 +35,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <NewExpense />
       <Expenses items={expenses} />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -52,5 +45,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
