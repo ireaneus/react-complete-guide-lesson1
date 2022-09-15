2c2106da9632176dabc7ea1e4c52b6dcbfe439b6
ireaneus <bwdave@protonmail.com>
2022-09-15 15:23:48 -0600

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,14 @@ export default function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
