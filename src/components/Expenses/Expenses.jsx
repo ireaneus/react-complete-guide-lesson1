cfa3122bc436bfb9ac0fd13711cebe5988525fe9
ireaneus <bwdave@protonmail.com>
2022-09-15 18:11:01 -0600

    Assignment 2

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './styles/Expenses.css';
+import ExpensesFilter from '../Expenses/ExpenseFilter';
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const selectedYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onSelectYear={selectedYearHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
